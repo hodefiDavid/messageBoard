f2fcf883120725c27468eafc5a4fc65d2af58879
david <davidhodefi0@gmail.com>
2021-04-07 20:32:02 +0300

    try to show other lines

--- Board.cpp
+++ Board.cpp
@@ -7,27 +7,47 @@
 
 
 using namespace std;
-string const ROW= "R";
-string const COL= "C";
+string const ROW = "R";
+string const COL = "C";
 
+void ariel::Board::post(unsigned int row, unsigned int col, ariel::Direction direction, const string &st) {
+    //check if the string is empty
+    if (st.length() == 0) {
+        return;
+    }
+    maxMinRowCol(row, col, direction, st.length());
+
+    for (char ch : st) {
+        string locationInMap;
+        locationInMap += ROW + to_string(row);
+        locationInMap += COL + to_string(col);
+
+        Board::mapBoard[locationInMap] = ch;
+
+        if (direction == Direction::Vertical) {
+            row++;
+        } else {
+            col++;
+        }
+
+    }
+}
 
 std::string ariel::Board::read(unsigned int row, unsigned int col, Direction direction, unsigned int readLength) {
     string ans;
-    while (readLength>0){
+    while (readLength > 0) {
         string locationInMap;
-        locationInMap += ROW+to_string(row);
-        locationInMap += COL+to_string(col);
+        locationInMap += ROW + to_string(row);
+        locationInMap += COL + to_string(col);
         if (direction == Direction::Vertical) {
             row++;
-        }
-        else{
+        } else {
             col++;
         }
-        if (mapBoard[locationInMap] != 0){
-            ans+= mapBoard[locationInMap];
-        }
-        else{
-            ans+='_';
+        if (mapBoard[locationInMap] != 0) {
+            ans += mapBoard[locationInMap];
+        } else {
+            ans += '_';
         }
         readLength--;
     }
@@ -36,81 +56,84 @@ std::string ariel::Board::read(unsigned int row, unsigned int col, Direction dir
     return ans;
 }
 
-void ariel::Board::post(unsigned int row, unsigned int col, ariel::Direction direction, const string &st) {
-   //check if the string is empty
-    if(st.length()==0){
-        return;
-    }
-    maxMinRowCol(row,col,direction,st.length());
-
-    for (char ch : st) {
-        string locationInMap;
-        locationInMap += ROW+to_string(row);
-        locationInMap += COL+to_string(col);
-
-        Board::mapBoard[locationInMap] = ch;
-
-        if (direction == Direction::Vertical) {
-            row++;
-        }
-        else{
-            col++;
-        }
-
-    }
-}
-
 void ariel::Board::show() {
+    unsigned int minusCol=0;
+    unsigned int minusRow=0;
+    unsigned int plusCol=0;
+    unsigned int plusRow=0;
 
     string board;
+    string boardEnd;
 
-    for (unsigned int row = minRow - 1; row <= maxRow + 1; ++row) {
-        board+=to_string(row)+": ";
-        for (unsigned int col = minCol - 1; col <= maxCol + 1 ; ++col) {
-            string locationInMap;
-            locationInMap += ROW+to_string(row);
-            locationInMap += COL+to_string(col);
-
-            if (mapBoard[locationInMap] != 0){
-                board+=mapBoard[locationInMap];
-            }
-            else{
-            board+="-";}
-        }
-        board+="\n";
-        }
-
-    std::cout<<board;
+    if (minRow!=0){
+        minusRow=1;
     }
+    else{
+        string underline = "_";
+        string temp = "_";
+        for (unsigned int col = minCol ; col <= maxCol; ++col){ temp+=underline;}
+        board += to_string(-1) + ": " +temp+"\n";
+    }
+    if (maxRow!=MAXUSINT){
+        plusRow=1;
+    }
+    else{
+        boardEnd += "4294967296 : ";
+    }
+    //we run from one row below the minimum row and until we reach one row
+    //leshnot et minus ahad lemasheho aher
+    for (unsigned int row = minRow - minusRow; row <= maxRow + plusRow; ++row) {
+        board += to_string(row) + ": ";
+        if (minCol!=0){
+            minusCol=1;
+        }
+        if (maxCol!=MAXUSINT){
+            plusRow=1;
+        }
+        for (unsigned int col = minCol - minusCol; col <= maxCol + maxCol; ++col) {
+            string locationInMap;
+            locationInMap += ROW + to_string(row);
+            locationInMap += COL + to_string(col);
+
+            if (mapBoard[locationInMap] != 0) {
+                board += mapBoard[locationInMap];
+            } else {
+                board += "-";
+            }
+        }
+        board += "\n";
+    }
+    board += boardEnd+"\n";
+
+    std::cout << board;
+}
 
 
 void ariel::Board::maxMinRowCol(unsigned int row, unsigned int col, ariel::Direction direction, unsigned long length) {
 
-    if(row<minRow){
-        minRow=row;
+    if (row < minRow) {
+        minRow = row;
     }
-    if(col<minCol){
-        minCol=col;
+    if (col < minCol) {
+        minCol = col;
     }
-    unsigned int addToRow=0;
-    unsigned int addToCol=0;
+    unsigned int addToRow = 0;
+    unsigned int addToCol = 0;
 
-    if (direction==Direction::Vertical){
-        addToRow+=length;
-    } else{
-        addToCol+=length;
-    }
-
-    if(row+addToRow>maxRow){
-        maxRow=row+addToRow;
-    }
-    if(col+addToCol>maxCol){
-        maxCol=col+addToCol;
+    if (direction == Direction::Vertical) {
+        addToRow += length;
+    } else {
+        addToCol += length;
     }
 
-}
-
-ariel::Board::Board(): maxCol(0), maxRow(0),minCol(MAXUSINT),minRow(MAXUSINT) {
+    if (row + addToRow > maxRow) {
+        maxRow = row + addToRow;
+    }
+    if (col + addToCol > maxCol) {
+        maxCol = col + addToCol;
+    }
 
 }
+//constructor that initialize the variables
+ariel::Board::Board() : maxCol(0), maxRow(0), minCol(MAXUSINT), minRow(MAXUSINT) {}
 
--- Board.hpp
+++ Board.hpp
@@ -9,33 +9,60 @@
 #include "Direction.hpp"
 unsigned int const MAXUSINT = 4294967295;
 namespace ariel {
-
+/**
+ * this class manage a bulletin board.
+ * in this bulletin board message can be writing over each other.
+ * The department support the following functions:
+ * post, read and show - you can read more about them in their description
+ */
     class Board {
     public:
         Board();
+        //Variables that hold the current minimum and maximum row and column, in order to print
+        //only the places that has been used
         unsigned int minRow;
         unsigned int maxRow;
         unsigned int minCol;
         unsigned int maxCol;
 
         std::map<std::string,char> mapBoard;
-
+        /**
+         * post - receives a position (line and column), direction (horizontal or vertical),
+         * and a string representing an ad, And post the string on the board in the appropriate place.
+         * @param row - unsigned int that represent the starting row number
+         * @param col - unsigned int that represent the starting column number
+         * @param direction - ariel::Direction thet represent direction Horizontal or Vertical
+         * @param st - const std::string that represent the message that the user want to post
+         */
         void post(unsigned int row, unsigned int col, ariel::Direction direction, const std::string &st);
 
         /**
-         *
-         * @param row
-         * @param col
-         * @param direction
-         * @param length
+         * read - receives a position (line and column), direction (horizontal or vertical),
+         * and a number of characters, reads what is written on the board in the nanton position
+         * and the length, and returns a string. The letter in each slot on the board is the last
+         * letter pasted there.
+         * @param row - unsigned int that represent the starting row number
+         * @param col - unsigned int that represent the starting column number
+         * @param direction - ariel::Direction thet represent direction Horizontal or Vertical
+         * @param length - length of chars you want to read (limited to the size of unsigned int)
          * @return
          */
         std::string read(unsigned row, unsigned col, ariel::Direction direction, unsigned length);
         /**
-         *
+         * Displays the current bulletin board in a convenient way to read.
+         * The function add underline (_) where the bulletin board empty.
+         * Displays only rows with messege inside them (not including the frame)
          */
         void show();
-
+        /**
+         * The function maxMinRowCol mange the minimum and maximum row and column.
+         * the function update the value of the variables according each post
+         *
+         * @param row - unsigned int that represent the row number
+         * @param col - unsigned int that represent the column number
+         * @param direction - ariel::Direction thet represent direction Horizontal or Vertical
+         * @param length - the length of chars the user posted
+         */
         void maxMinRowCol(unsigned int row, unsigned int col, Direction direction, unsigned long length);
     };
 }
